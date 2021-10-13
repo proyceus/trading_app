493c94a947252173dd15565892baabe314ed1523
Pierre Royce <proyceus@gmail.com>
2021-10-13 13:22:54 -0400

    changed name of portfolio to exchanges, will create separate portfolio tab

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import './Navbar.css';
@@ -6,16 +6,17 @@ import {TradeContainer, ChartContainer, Assets} from '../index.js';
 
 const Navbar = () => {
 
+  const [tradingAsset, setTradingAsset] = useState('btcusdt');
 
   return (
     <div>
       <Tabs defaultActiveKey="trade" id="uncontrolled-tab-example" className="mb-3">
-        <Tab eventKey="portfolio" title="Portfolio">
-          <Assets />
+        <Tab eventKey="exchanges" title="Exchanges">
+          <Assets changeAsset={setTradingAsset} />
         </Tab>
         <Tab eventKey="trade" title="Trade">
-          {/* <ChartContainer /> */}
-          <TradeContainer />
+          <ChartContainer asset={tradingAsset} />
+          <TradeContainer asset={tradingAsset} />
         </Tab>
       </Tabs>
     </div>
