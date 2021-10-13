c5a7030ccf4933291fda7365256d769f9d8fe094
Pierre Royce <proyceus@gmail.com>
2021-10-13 14:55:44 -0400

    added in a dropdown for market/limit orders

--- client/src/components/TradeContainer/TradeContainer.js
+++ client/src/components/TradeContainer/TradeContainer.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TradeContainer.css';
 import Button from 'react-bootstrap/Button';
 
 const TradeContainer = () => {
 
+  //default market order type to market
+  const [orderType, setOrderType] = useState('market');
 
   return (
     <div className="trade-container">
@@ -12,6 +14,11 @@ const TradeContainer = () => {
       <div className="break"></div>
       <input type="text" placeholder="price"></input>
       <input type="text" placeholder="amount"></input>
+      <div className="break"></div>
+      <select>
+        <option value="market">Market Order</option>
+        <option value="limit">Limit Order</option>
+      </select>
       <div className="break"></div>
       <Button variant="success" size="lg" className="trade-button">Buy</Button>
       <Button variant="danger" size="lg" className="trade-button">Sell</Button>
