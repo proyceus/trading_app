bb2482f1d3d23bb47cb5e5c4e8e98bd698330dcc
Pierre Royce <proyceus@gmail.com>
2021-10-13 13:21:31 -0400

    added in Binance, Kucoin and FTX US API to get dropdown of all available tokenpairs

--- client/src/components/AssetDropdown/AssetDropdown.js
+++ client/src/components/AssetDropdown/AssetDropdown.js
@@ -2,19 +2,24 @@ import React, {useEffect, useState} from 'react';
 import Select from 'react-select';
 import './AssetDropdown.css';
 
-const AssetDropdown = ({assetList}) => {
+const AssetDropdown = ({assetList, changeAsset}) => {
 
-  const [dropdownOptions, setdropdownOptions] = useState([]);
+  const [dropdownOptions, setDropdownOptions] = useState([]);
 
   useEffect(() => {
-    assetList.forEach((item) => setdropdownOptions(prevState => [...prevState, {value: item.symbol, label: item.baseAsset + '/' + item.quoteAsset}]))
+    assetList[0].quoteAsset !== undefined ? assetList.forEach((item) => setDropdownOptions(prevState => [...prevState, {value: item.baseAsset + item.quoteAsset, label: item.baseAsset + '/' + item.quoteAsset}])) : assetList.forEach((item) => setDropdownOptions(prevState => [...prevState, {value: item.symbol, label: item.baseCurrency + '/' + item.quoteCurrency}]))
   }, [])
 
   //add filter option
 
+  const onChange = selectedOption => {
+    const filteredSelection = selectedOption.label.replace(/[^a-zA-Z ]/g, "");
+    changeAsset(filteredSelection);
+  }
+
   return (
     <div>
-      <Select options={dropdownOptions} />
+      <Select options={dropdownOptions} onChange={onChange}/>
     </div>
 
   )
